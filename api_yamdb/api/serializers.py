f601b87476d3046cc26ed99bdfa875abfb1ac700
Александра Румянцева <alexrumyantceva.gamil.com@yandex.ru>
2022-06-22 20:27:48 +0300

    Start

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Comment, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'author', 'pub_date', 'score', 'text')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'pub_date', 'text')
+
+    def validate_score(self, value):
+        if (0 <= value < 10):
+            raise serializers.ValidationError('Введите оценку от 1 до 10')
+        return value
+
+    def validate(self, data):
+        if Review.objects.filter(
+                title=self.context['view'].kwargs['title_id'],
+                author=self.context['request'].user
+        ).exists() and self.context['request'].method == 'POST':
+            raise serializers.ValidationError(
+                'Вы уже оставляли здесь отзыв!')
+        return data
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        constraints = [models.UniqueConstraint(
+            fields=['title', 'author'],
+            name='unique_review',
+        )]
+
+
+class Comment(models.Model):
+    rewiev = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.review}, {self.text}'
