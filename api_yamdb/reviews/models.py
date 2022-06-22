f19027924854c135785dca4fde9fc52316aee4dd
Andrew Bondarenko <adementev78@gmail.com>
2022-06-22 19:14:14 +0300

    Sprint10 v1.1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from reviews.validators import validate_year
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Категория',
+        help_text='Выберите категорию'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Слаг',
+        help_text='Укажите слаг'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('-id',)
+
+    def str(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Жанр',
+        help_text='Название жанра'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Слаг',
+        help_text='Укажите слаг'
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        ordering = ('-id',)
+
+    def str(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Произведение',
+        help_text='Введите название произведения',
+        db_index=True
+    )
+    year = models.SmallIntegerField(
+        verbose_name='Год выпуска',
+        help_text='Введите год выпуска',
+        validators=[validate_year],  # TO DO вывести в отдельный файл
+        blank=True
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        help_text='Описание произведения',
+        max_length=250,
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='titles',
+        verbose_name='Жанр',
+        help_text='Укажите жанр'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='titles',
+        verbose_name='Категория',
+        help_text='Укажите категорию'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+        ordering = ('-id',)
+
+    def str(self):
+        return self.name[:15]
