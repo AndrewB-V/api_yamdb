679c3aa59d97ebeaef509c3e753cefd1a920540f
Andrew Bondarenko <adementev78@gmail.com>
2022-06-28 13:25:11 +0300

    Sprint10 v2.0.1

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,10 @@
 from rest_framework import routers
-
 from django.urls import path, include
 from rest_framework.authtoken import views
-
 from api.views import (
     ReviewViewSet, CommentViewSet,
     TitleViewSet, CategoryViewSet,
-    GenreViewSet, UsersViewSet)
+    GenreViewSet, UserViewSet, user_register, token_create, )
 
 app_name = 'api'
 
@@ -21,10 +19,10 @@ router_v1.register(
     r'titles/(?P<titles_id>\d+)/reviews/(?P<reviews_id>\d+)/comments',
     CommentViewSet, basename='comments'
 )
-router_v1.register('users', UsersViewSet, basename='users')
+router_v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('v1/auth/signup/', views.user_register, name='register'),
-    path('v1/auth/token/', views.token_create, name='token'),
+    path('v1/auth/signup/', user_register, name='register'),
+    path('v1/auth/token/', token_create, name='token'),
 ]
